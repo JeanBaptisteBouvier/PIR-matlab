878393511fdfcf9cce748295c83f90c57421b846
JeanBaptisteBouvier <jeanbaptistebouvier@orange.fr>
2017-06-08 13:46:54 +0200

    update without theta

--- rdv/line_of_sight_corridor.m
+++ rdv/line_of_sight_corridor.m
@@ -1,28 +1,26 @@
-function output = line_of_sight_corridor( alpha, beta, phi, theta, TOF , x0, min_dist)
+function output = line_of_sight_corridor( alpha, beta, phi, TOF , x0, min_dist)
 % This Function compute the hold points in LVLH that are used in the Rendezvous code
 % 
 % alpha and beta are two trigger angles [rad]
-% theta is the approach angle [rad]
 % phi is an offset angle less than either trigger angle [rad]
 % TOF is the time of flight [s]
 % x0 is the position when the chaserfirst arrives at the corridor [km]
 % min_dist is the minimum distance at which the chaser stop to maneuver [km]
 
 
-hold_points = [];
 delta_T = [];
 delta_X = abs(x0(1) - min_dist * cos((alpha+beta)/2)); % delta_X is the x-distance of the chaser from the target in LVLH that is the distance travelled in the corridor 
 
-% Chech whether the Chaser is initially outside the corridor
+% Check whether the Chaser is initially outside the corridor
 rel_angl = atan(x0(3)/x0(1));
-s = x0(1)/abs(x0(1)); % signe of x0(1)
+s = x0(1)/abs(x0(1)); % sign of x0(1)
 if 0 < rel_angl && rel_angl < beta % in the upper corridor
-    n=1;
-    hold_points = [s*abs(x0(3))/tan(beta) 0 x0(3)];
+    n = 1;
+    hold_points = [s*abs(x0(3))/tan(beta) 0 x0(3)]; %first point straight ahead from the Chaser
     
 elseif  0 > rel_angl && rel_angl > -alpha % in the lower corridor
-    n=0;
-    hold_points = [s*abs(x0(3))/tan(alpha) 0 x0(3)];
+    n = 0;
+    hold_points = [s*abs(x0(3))/tan(alpha) 0 x0(3)]; %first point straight ahead from the Chaser
     
 else % outside
     error('The chaser is initially out of the corridor, redefine parameters') 
@@ -54,8 +52,9 @@ output.delta_T = delta_T;
 figure
 hold on
 grid on
-plot([A0*cos(alpha) -A0*cos(alpha)], [A0*sin(alpha) -A0*sin(alpha)], ':g')
-plot([A0*cos(alpha) -A0*cos(alpha)], [-A0*sin(alpha) A0*sin(alpha)], ':g')
+plot(x0(1), x0(3), '*g') % Chaser initial position
+plot([A0*cos(alpha) -A0*cos(alpha)], [A0*sin(alpha) -A0*sin(alpha)], ':r') % Corridor
+plot([A0*cos(alpha) -A0*cos(alpha)], [-A0*sin(alpha) A0*sin(alpha)], ':r') % Corridor
 plot([x0(1) hold_points(1,1)], [x0(3) hold_points(1,3)], 'b')
 for k = 1:n-1
     plot(hold_points(k,1), hold_points(k,3), '*k')
