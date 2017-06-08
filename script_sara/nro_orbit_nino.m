799a013ae52894aee6119346a4669228c16d734a
sara0493 <sara-04@hotmail.it>
2017-06-08 15:35:32 +0200

    Add files via upload

--- script_sara/nro_orbit_nino.m
+++ script_sara/nro_orbit_nino.m
@@ -26,7 +26,7 @@ nro_T = nro_interpolation(cr3bp, nro_T, nro_init_EML2, default, cst, 'Az', 70000
 nro_C = init_nro(cr3bp, cr3bp.l2, cst.orbit.family.SOUTHERN, cst);
 
 % Interpolation and plot
-nro_C = nro_interpolation(cr3bp, nro_C, nro_init_EML2, default, cst, 'Az', 69900);
+nro_C = nro_interpolation(cr3bp, nro_C, nro_init_EML2, default, cst, 'Az', 69960);
 
 
 %% Nonlinear Relative dynamics in synodic frame
@@ -42,10 +42,31 @@ theta_C = 178*pi/180;
 choice = [1 0 0 0]; %[1 0 0 0] CW, [0 1 0 0] SL, [0 0 1 0] LR, [1 1 1 0] previous 3, [0 0 0 1] continuation
 
 % selection of hold points and TOFs
-hold_points_dim = [-5 0 -1];
-TOF_dim  = [3600*2 3600];
-TOF_cont = [3600 600]; 
+% hold_points_dim = [-5 0 -1];
+% TOF_dim  = [3600*2 3600];
+% TOF_cont = [3600 600]; 
+
+t_T = interp1(nro_T.alpha, nro_T.tv, theta_T, 'spline');
+t_C = interp1(nro_C.alpha, nro_C.tv, theta_C, 'spline');
+x0_T   = state_time(t_T,nro_T)';  %initial position of the target in the synodic frame
+x0_C   = state_time(t_C,nro_C)';  %initial position of the chaser in the synodic frame
+x0_rel = x0_C - x0_T;             %synodic frame 
+x0_LVLH     = rsyn2rlvlh(t_T, x0_rel, x0_T, cr3bp.mu);   %relative vector in LVLH with z-axis toward the Moon.
+x0 = x0_LVLH(1:3)*cr3bp.L;
+
+alpha = 35*pi/180;
+beta = 35*pi/180;
+phi = 0.5*pi/180;
+min_dist = 0.5;
+TOF = 3600*10;
+% out =  line_of_sight_corridor( alpha, beta, phi, TOF , x0, min_dist);
+out = line_of_sight_glide(alpha, phi, TOF, x0, min_dist);
+hold_points_dim = out.hold_points;
+% hold_points_dim = [-5 1 0]; % control point in the LVLH framework
+TOF_dim  = [out.delta_T; 3600];
+TOF_cont = TOF_dim./2;
 int_time = 300;
+
 
 % other settings
 intervals = 100; %n di intervalli in cui è diviso il TOF per Luquette & CW
@@ -67,7 +88,7 @@ settings.it_max = it_max;
 settings.options = options;
 settings.cr3bp = cr3bp;
 
-output_ch = Rendezvous_choice2(choice, hold_points_dim, TOF_dim, init_cond, orbits, settings, continuation, 1);
+output_ch = Rendezvous_choice_l(choice, hold_points_dim, TOF_dim, init_cond, orbits, settings, continuation, 1);
 
 
 
